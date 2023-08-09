836d40b86f069ec83b46cd56bdec516603e3883e
Hemanth_Jain <hemanthjain06101996@gmail.com>
2023-08-09 09:32:06 +0000

    File is added

---
+++ 10lines.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source_dir="/home/ec2-user/mock"
+destination_dir="/home/ec2-user/scripts/interview"
+
+if [ ! -d "$destination_dir" ];
+then
+    mkdir -p "$destination_dir"
+fi
+
+for file in "$source_dir"/*;
+do
+    if [ -f "$file" ];
+    then
+        line_count=$(wc -l < "$file")
+        if [ "$line_count" -eq 10 ];
+       	then
+            mv "$file" "$destination_dir"
+            echo "Moved $file to $destination_dir"
+        fi
+    fi
+done
+
